1661ef7dd9d46b0cba90091ee5bf623c7b64c6b7
Tomi Ikonen <tomi.ikonen@gmail.com>
2021-06-19 22:29:34 -0600

    add static/image directory and fileserver in routes.go

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,8 @@ func Routes(app *config.AppConfig) http.Handler {
 	mux.Use(session.LoadAndSave)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
