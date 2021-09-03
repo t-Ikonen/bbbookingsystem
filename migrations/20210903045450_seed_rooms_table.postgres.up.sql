406c6102a557fc1987ce93425f5609740e18ddc8
tikonen <tomi.ikonen@gmail.com>
2021-09-02 23:04:19 -0600

    last DB changes

---
+++ migrations/20210903045450_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.rooms (room_name,shower,minibar,pricing_id,created_at,updated_at) VALUES
+	 ('Frost Suite',true,true,1,'2021-05-08 00:00:00.000','2021-05-08 00:00:00.000'),
+	 ('Snow Suite',true,true,2,'2021-09-08 00:00:00.000','2021-09-08 00:00:00.000'),
+	 ('Northern Lights Cabin',true,true,3,'2021-08-09 00:00:00.000','2021-08-08 00:00:00.000');
